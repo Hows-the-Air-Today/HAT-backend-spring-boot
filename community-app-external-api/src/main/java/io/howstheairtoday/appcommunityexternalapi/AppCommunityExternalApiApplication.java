646aee7aa6a0913be32aa5c802567083e5c5d84b
jhva <junghun8158@naver.com>
2023-03-18 17:57:33 +0900

    :tada: build(HAT-112) : 멀티모듈 의존관계설정

--- community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/AppCommunityExternalApiApplication.java
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/AppCommunityExternalApiApplication.java
@@ -3,11 +3,13 @@ package io.howstheairtoday.appcommunityexternalapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AppCommunityExternalApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppCommunityExternalApiApplication.class, args);
+
     }
 
 }
