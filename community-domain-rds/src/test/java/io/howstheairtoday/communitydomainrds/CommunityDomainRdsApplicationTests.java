ea3fd221d8ff837f6c7f09b96306a01d16b95f97
jhva <junghun8158@naver.com>
2023-03-19 02:35:32 +0900

    :bug:  fix: Fixed the PR(5) part that failed to build

--- community-domain-rds/src/test/java/io/howstheairtoday/communitydomainrds/CommunityDomainRdsApplicationTests.java
+++ community-domain-rds/src/test/java/io/howstheairtoday/communitydomainrds/CommunityDomainRdsApplicationTests.java
@@ -2,8 +2,10 @@ package io.howstheairtoday.communitydomainrds;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("kjh")
 class CommunityDomainRdsApplicationTests {
 
 	@Test
