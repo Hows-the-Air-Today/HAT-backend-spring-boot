72e94dacd63fe3571ecb12bd1dfd38ee060ed55d
Hyyena <sjtxm0320@gmail.com>
2023-03-21 03:47:10 +0900

    🐛 fix: 테스트 코드 어노테이션 수정

--- air-quality-app-batch/src/test/java/io/howstheairtoday/airqualityappbatch/AirQualityAppBatchApplicationTests.java
+++ air-quality-app-batch/src/test/java/io/howstheairtoday/airqualityappbatch/AirQualityAppBatchApplicationTests.java
@@ -1,9 +1,10 @@
 package io.howstheairtoday.airqualityappbatch;
 
+import io.howstheairtoday.AirQualityAppBatchApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AirQualityAppBatchApplication.class)
 class AirQualityAppBatchApplicationTests {
 
     @Test
