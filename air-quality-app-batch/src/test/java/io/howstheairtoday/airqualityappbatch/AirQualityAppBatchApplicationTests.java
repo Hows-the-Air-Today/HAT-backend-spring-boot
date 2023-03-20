f9c06bfacdae4bb983841b8570f0bbb990dfa347
Hyyena <sjtxm0320@gmail.com>
2023-03-21 04:25:16 +0900

    🐛 fix: 테스트 코드 어노테이션 추가

--- air-quality-app-batch/src/test/java/io/howstheairtoday/airqualityappbatch/AirQualityAppBatchApplicationTests.java
+++ air-quality-app-batch/src/test/java/io/howstheairtoday/airqualityappbatch/AirQualityAppBatchApplicationTests.java
@@ -1,10 +1,12 @@
 package io.howstheairtoday.airqualityappbatch;
 
-import io.howstheairtoday.AirQualityAppBatchApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = AirQualityAppBatchApplication.class)
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class AirQualityAppBatchApplicationTests {
 
     @Test
