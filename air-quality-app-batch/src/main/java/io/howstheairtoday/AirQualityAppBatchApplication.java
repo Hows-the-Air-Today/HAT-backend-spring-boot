c3cc79070370934c816144a8726b73434406cc00
Hyyena <sjtxm0320@gmail.com>
2023-03-20 19:44:44 +0900

    ✅ test: 테스트 프로필 활성화 어노테이션 추가

--- air-quality-app-batch/src/main/java/io/howstheairtoday/AirQualityAppBatchApplication.java
+++ air-quality-app-batch/src/main/java/io/howstheairtoday/AirQualityAppBatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppAirQualityBatchApplication {
+public class AirQualityAppBatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppAirQualityBatchApplication.class, args);
+        SpringApplication.run(AirQualityAppBatchApplication.class, args);
     }
 
 }
--- community-domain-rds/src/test/java/io/howstheairtoday/communitydomainrds/CommunityDomainRdsApplicationTests.java
+++ community-domain-rds/src/test/java/io/howstheairtoday/communitydomainrds/CommunityDomainRdsApplicationTests.java
@@ -2,12 +2,14 @@ package io.howstheairtoday.communitydomainrds;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CommunityDomainRdsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
