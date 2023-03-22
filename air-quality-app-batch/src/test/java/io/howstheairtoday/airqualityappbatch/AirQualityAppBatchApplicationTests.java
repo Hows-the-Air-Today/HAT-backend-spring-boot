621ceebccf1498ceefd9ceb21dce1cfa561fdf49
Hyyena <70932170+Hyyena@users.noreply.github.com>
2023-03-22 21:51:42 +0900

    🚑 fix(HAT-133): Gradle 빌드 실패 에러 해결

--- air-quality-app-batch/src/test/java/io/howstheairtoday/airqualityappbatch/AirQualityAppBatchApplicationTests.java
+++ air-quality-app-batch/src/test/java/io/howstheairtoday/airqualityappbatch/AirQualityAppBatchApplicationTests.java
@@ -1,11 +1,12 @@
 package io.howstheairtoday.airqualityappbatch;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class AirQualityAppBatchApplicationTests {
 
     @Test
--- air-quality-domain-service/src/test/java/io/howstheairtoday/airqualitydomainservice/AirQualityDomainServiceApplicationTests.java
+++ air-quality-domain-service/src/test/java/io/howstheairtoday/airqualitydomainservice/AirQualityDomainServiceApplicationTests.java
@@ -2,10 +2,8 @@ package io.howstheairtoday.airqualitydomainservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class AirQualityDomainServiceApplicationTests {
 
     @Test
