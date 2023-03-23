9961ec3ca760e5ad2339defb9677ed0d197221e3
jhva <junghun8158@naver.com>
2023-03-24 04:52:42 +0900

    🐛 fix: 도메인 모듈 jpa 테스트 환경을 위한 어노테이션추가

--- community-domain-rds/src/test/java/io/howstheairtoday/communitydomainrds/repository/PostRepositoryTest.java
+++ community-domain-rds/src/test/java/io/howstheairtoday/communitydomainrds/repository/PostRepositoryTest.java
@@ -6,14 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 import io.howstheairtoday.communitydomainrds.entity.Post;
 import io.howstheairtoday.communitydomainrds.entity.PostImage;
@@ -21,12 +24,15 @@ import io.howstheairtoday.communitydomainrds.entity.PostImage;
 @DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(classes = {PostRepositoryTest.class})
+@EnableJpaRepositories(basePackages = "io.howstheairtoday.communitydomainrds.repository")
+@EntityScan("io.howstheairtoday.communitydomainrds.entity")
 public class PostRepositoryTest {
 
     @Autowired
     private PostRepository postRepository;
 
-    @DisplayName("게시물 삽입을 할때 테스트 URL을 3개를 넣고 작성")
+    @DisplayName("게시물 삽입을 할때 테스트 URL 을 3개를 넣고 작성")
     @Test
     public void savePost() {
         //given
@@ -51,6 +57,7 @@ public class PostRepositoryTest {
 
         //then
         assertSame(post, savePost);
+
         Assertions.assertThat(savePost.getContent()).isEqualTo("게시글 내용");
         Assertions.assertThat(savePost.getRegion()).isEqualTo("동남로");
         Assertions.assertThat(savePost.getImageArray().get(0).getPostImageNumber()).isEqualTo(0);
