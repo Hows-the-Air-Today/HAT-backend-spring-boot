1c324db95aa6a4a112f90cc226354cbbe37db9d2
jhva <junghun8158@naver.com>
2023-03-24 20:52:51 +0900

    ✨ feat(HAT-122): utils ApiResponse 추가

---
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/common/ApiResponse.java
@@ -0,0 +1,35 @@
+package io.howstheairtoday.appcommunityexternalapi.common;
+
+import org.springframework.http.HttpStatusCode;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Builder
+public class ApiResponse<T> {
+
+    private String msg;
+    private Integer code;
+    private T data;
+
+    public static <T> ApiResponse<T> res(final Integer statusCode, final String msg) {
+        return ApiResponse.<T>builder()
+            .msg(msg)
+            .code(statusCode)
+            .build();
+    }
+
+    public static <T> ApiResponse<T> res(final Integer statusCode, final String msg, final T data) {
+        return ApiResponse.<T>builder()
+            .msg(msg)
+            .code(statusCode)
+            .data(data)
+            .build();
+    }
+}
