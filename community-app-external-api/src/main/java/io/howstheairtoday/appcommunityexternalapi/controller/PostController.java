e8dd855600d356a933a11f38c2ccc30c464cccb9
jhva <junghun8158@naver.com>
2023-03-24 20:59:16 +0900

    ✨ feat(HAT-121): 게시글 controller 로직 추가

---
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/controller/PostController.java
@@ -0,0 +1,28 @@
+package io.howstheairtoday.appcommunityexternalapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.howstheairtoday.appcommunityexternalapi.common.ApiResponse;
+import io.howstheairtoday.appcommunityexternalapi.service.ExternalPostService;
+import io.howstheairtoday.appcommunityexternalapi.service.dto.request.PostRequestDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@RestController
+public class PostController {
+
+    private final ExternalPostService externalPostService;
+
+    @PostMapping("/post")
+    public ApiResponse<Object> createFeed(@Valid @RequestBody PostRequestDto.SaveRequestDto saveRequestDto) {
+        externalPostService.createPost(saveRequestDto);
+        return ApiResponse.res(HttpStatus.CREATED.value(), "success");
+    }
+}
