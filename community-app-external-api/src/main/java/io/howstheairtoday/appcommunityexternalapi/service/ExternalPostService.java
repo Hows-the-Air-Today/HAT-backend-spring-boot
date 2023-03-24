1c512ba22088375d8760ac3c8b461a0748cc1368
jhva <junghun8158@naver.com>
2023-03-24 20:55:26 +0900

    ✨ feat(HAT-120): api-module 게시글 service 로직 구현

---
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/service/ExternalPostService.java
@@ -0,0 +1,33 @@
+package io.howstheairtoday.appcommunityexternalapi.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import io.howstheairtoday.appcommunityexternalapi.service.dto.request.PostRequestDto;
+import io.howstheairtoday.communitydomainrds.entity.Post;
+import io.howstheairtoday.communitydomainrds.entity.PostImage;
+import io.howstheairtoday.communitydomainrds.service.DomainCommunityService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ExternalPostService {
+
+    private final DomainCommunityService domainCommunityService;
+
+    public void createPost(PostRequestDto.SaveRequestDto saveRequestDto) {
+
+        List<PostRequestDto.PostImagesDto> postImages = saveRequestDto.getPostImageDtoList();
+
+        Post post = Post.createPost(saveRequestDto.getContent(), saveRequestDto.getLocation());
+
+        postImages.forEach(
+            postImg -> post.imagesAdd(
+                PostImage.createImages(postImg.getPostImageNumber(), postImg.getPostImageUrl(), post)));
+
+        domainCommunityService.savePost(post);
+    }
+
+}
