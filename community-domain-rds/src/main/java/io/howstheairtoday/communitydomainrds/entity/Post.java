9148672e93c40a06a07e0c897b83b8dfbbe84f02
jhva <junghun8158@naver.com>
2023-03-24 20:58:10 +0900

    ♻️ refactor: api 문서와 다른 필드 및 PostService 테스팅 후 에 따른 리팩토링

--- community-domain-rds/src/main/java/io/howstheairtoday/communitydomainrds/entity/Post.java
+++ community-domain-rds/src/main/java/io/howstheairtoday/communitydomainrds/entity/Post.java
@@ -1,13 +1,17 @@
 package io.howstheairtoday.communitydomainrds.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import io.howstheairtoday.communitydomainrds.common.BaseTimeEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -35,29 +39,40 @@ public class Post extends BaseTimeEntity {
 
     private String content;
 
-    private String region;
+    private String location;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
     @Getter
     private List<PostImage> imageArray;
 
     @Builder
-    public Post(UUID id, String content, String region, UUID userId, List<PostImage> imageArray) {
+    public Post(UUID id, String content, String location, UUID userId) {
         this.userId = userId;
         this.content = content;
-        this.region = region;
-        this.imageArray = imageArray;
+        this.location = location;
+        this.imageArray = new ArrayList<>();
+    }
+
+    public static Post createPost(String content, String location) {
+        return Post.builder()
+            .location(location)
+            .content(content)
+            .build();
     }
 
     public void updateFeed(Post post, List<PostImage> imageArray) {
         this.content = post.getContent();
-        this.region = post.getRegion();
+        this.location = post.getLocation();
         this.imageArray = imageArray;
     }
 
     public void deletePost() {
         this.setDeletedAt(LocalDateTime.now());
         imageArray.forEach(images -> images.setDeletedAt(LocalDateTime.now()));
+    }
+
+    public void imagesAdd(PostImage postImage) {
+        this.imageArray.add(postImage);
     }
 
 }
