4bd7ac6e62393d3e61d4a6a291ec014f4c536771
jhva <junghun8158@naver.com>
2023-03-25 19:46:01 +0900

    🚑️ fix :Lint오류

--- community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/common/ApiResponse.java
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/common/ApiResponse.java
@@ -1,14 +1,11 @@
 package io.howstheairtoday.appcommunityexternalapi.common;
 
-import org.springframework.http.HttpStatusCode;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @Builder
