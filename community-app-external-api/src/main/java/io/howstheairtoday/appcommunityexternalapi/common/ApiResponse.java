c70fea284c8654a7e9b254f132553187bb5ce43b
jhva <junghun8158@naver.com>
2023-03-25 19:57:17 +0900

    🚑️ fix :Lint(3)오류

--- community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/common/ApiResponse.java
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/common/ApiResponse.java
@@ -10,10 +10,16 @@ import lombok.Data;
 @Builder
 public class ApiResponse<T> {
 
-    private String msg;
-    private Integer statusCode;
-    private T data;
+    private String msg;  // 에러 메시지 또는 성공 메시지
+    private Integer statusCode;  // HTTP 상태 코드
+    private T data;  // 데이터
 
+    /**
+     * HTTP 상태 코드와 에러 메시지만 있는 ApiResponse 객체를 반환합니다.
+     * @param statusCode HTTP 상태 코드
+     * @param msg 에러 메시지
+     * @return ApiResponse 객체
+     */
     public static <T> ApiResponse<T> res(final Integer statusCode, final String msg) {
         return ApiResponse.<T>builder()
             .msg(msg)
@@ -21,6 +27,13 @@ public class ApiResponse<T> {
             .build();
     }
 
+    /**
+     * HTTP 상태 코드와 에러 메시지, 데이터를 가진 ApiResponse 객체를 반환합니다.
+     * @param statusCode HTTP 상태 코드
+     * @param msg 에러 메시지
+     * @param data 데이터
+     * @return ApiResponse 객체
+     */
     public static <T> ApiResponse<T> res(final Integer statusCode, final String msg, final T data) {
         return ApiResponse.<T>builder()
             .msg(msg)
@@ -29,3 +42,4 @@ public class ApiResponse<T> {
             .build();
     }
 }
+
