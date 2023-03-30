baba4c542a63fb572b534a4943efaf66cfe25467
jhva <junghun8158@naver.com>
2023-03-30 21:22:53 +0900

    ✨ feat(HAT-164): 게시글 수정 컨트롤러 기능 추가

--- community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/controller/PostController.java
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/controller/PostController.java
@@ -1,6 +1,10 @@
 package io.howstheairtoday.appcommunityexternalapi.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +37,17 @@ public class PostController {
         externalPostService.createPost(saveRequestDto);
         return ApiResponse.<Object>builder()
             .statusCode(HttpStatus.CREATED.value())
+            .msg("success")
+            .build();
+    }
+
+    @PatchMapping("/post/{postsId}")
+    public ApiResponse<Object> updatePost(@Valid @RequestBody final PostRequestDto.SaveRequestDto saveRequestDto,
+        @PathVariable UUID postsId
+    ) {
+        externalPostService.updatePost(saveRequestDto, postsId);
+        return ApiResponse.<Object>builder()
+            .statusCode(HttpStatus.OK.value())
             .msg("success")
             .build();
     }
