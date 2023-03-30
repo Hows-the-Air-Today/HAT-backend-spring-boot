ec7e9eba0b099e441579aa66570b8465b73d77b6
jhva <junghun8158@naver.com>
2023-03-30 21:22:11 +0900

    ♻️ refector: 각 모듈의 독립적인 서비스를 위한 findById 메서드추가

--- community-domain-rds/src/main/java/io/howstheairtoday/communitydomainrds/repository/PostRepository.java
+++ community-domain-rds/src/main/java/io/howstheairtoday/communitydomainrds/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package io.howstheairtoday.communitydomainrds.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import io.howstheairtoday.communitydomainrds.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
+
+    Optional<Post> findById(UUID uuid);
 }
