017537bab152bb9a67ab911797948c97d19501d9
youngsoosoo <lovepys01@naver.com>
2023-03-31 14:00:59 +0900

    ✨ feat(HAT-77): 빌드 오류 수정

--- air-quality-app-batch/src/test/java/io/howstheairtoday/airqualityappbatch/AirQualityAppBatchApplicationTests.java
+++ air-quality-app-batch/src/test/java/io/howstheairtoday/airqualityappbatch/AirQualityAppBatchApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-// @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class AirQualityAppBatchApplicationTests {
 
     @Test
