c30a7ceccff85ae55fbd707371e79c72db2aa32f
IToriginal <gitshineit@gmail.com>
2023-04-01 01:05:06 +0900

    ✨ feat(HAT-100): 회원 생성 Controller 클래스 생성

---
+++ member-app-external-api/src/main/java/io/howstheairtoday/memberappexternalapi/common/ApiResponse.java
@@ -0,0 +1,58 @@
+package io.howstheairtoday.memberappexternalapi.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * API 응답 객체.
+ *
+ * @param <T> 데이터 타입.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Builder
+public class ApiResponse<T> {
+
+    /** 응답 메시지. */
+    private String msg;
+
+    /** 응답 코드. */
+    private Integer statusCode;
+
+    /** 데이터. */
+    private T data;
+
+    /**
+     * 응답 객체 생성.
+     *
+     * @param <T> 데이터 타입.
+     * @param statusCode 응답 코드.
+     * @param msg 응답 메시지.
+     * @return ApiResponse 객체.
+     */
+    public static <T> ApiResponse<T> res(final Integer statusCode, final String msg) {
+        return ApiResponse.<T>builder()
+            .statusCode(statusCode)
+            .msg(msg)
+            .build();
+    }
+
+    /**
+     * 응답 객체 생성.
+     *
+     * @param <T> 데이터 타입.
+     * @param statusCode 응답 코드.
+     * @param msg 응답 메시지.
+     * @param data 데이터.
+     * @return ApiResponse 객체.
+     */
+    public static <T> ApiResponse<T> res(final Integer statusCode, final String msg, final T data) {
+        return ApiResponse.<T>builder()
+            .statusCode(statusCode)
+            .msg(msg)
+            .data(data)
+            .build();
+    }
+}
