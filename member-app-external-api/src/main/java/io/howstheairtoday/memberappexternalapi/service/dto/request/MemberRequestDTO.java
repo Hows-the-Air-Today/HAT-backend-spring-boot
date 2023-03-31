1129f74b912e7425f83462db6daa29257bd4715c
IToriginal <gitshineit@gmail.com>
2023-04-01 01:03:48 +0900

    ✨ feat(HAT-175): 회원 생성 RequestDTO 클래스 생성

---
+++ member-app-external-api/src/main/java/io/howstheairtoday/memberappexternalapi/service/dto/request/MemberRequestDTO.java
@@ -0,0 +1,48 @@
+package io.howstheairtoday.memberappexternalapi.service.dto.request;
+
+import io.howstheairtoday.memberdomainrds.entity.LoginRole;
+import io.howstheairtoday.memberdomainrds.entity.LoginType;
+import io.howstheairtoday.memberdomainrds.entity.Member;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MemberRequestDTO {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Builder
+    public static class SaveMemberRequestDto {
+
+        /**
+         * 회원가입시 필요한 내용
+         */
+        @NotBlank(message = "아이디를 입력해 주세요.")
+        private String loginId;
+
+        @NotEmpty(message = "비밀번호를 입력해 주세요.")
+        private String loginPassword;
+
+        @NotBlank(message = "인증 가능한 이메일을 입력해 주세요.")
+        private String email;
+
+        @NotBlank(message = "사용하실 닉네임을 입력해 주세요.")
+        private String nickname;
+
+        public Member toEntity() {
+            return Member.builder()
+                .loginId(loginId)
+                .loginPassword(loginPassword)
+                .email(email)
+                .nickname(nickname)
+                .memberProfileImage("default.jpg")
+                .loginType(LoginType.LOCAL)
+                .loginRole(LoginRole.ROLE_USER)
+                .build();
+        }
+    }
+}
