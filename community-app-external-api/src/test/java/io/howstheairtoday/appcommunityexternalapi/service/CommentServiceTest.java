5e6dd5ed7b023d048545e5c5d934110ef6004ddf
joo <jooh9992@gmail.com>
2023-04-11 15:18:47 +0900

    ✨ feat(HAT-210): 댓글 조회 service 테스트

--- community-app-external-api/src/test/java/io/howstheairtoday/appcommunityexternalapi/service/CommentServiceTest.java
+++ community-app-external-api/src/test/java/io/howstheairtoday/appcommunityexternalapi/service/CommentServiceTest.java
@@ -5,13 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
 import io.howstheairtoday.appcommunityexternalapi.service.dto.request.CommentRequestDTO;
+import io.howstheairtoday.appcommunityexternalapi.service.dto.response.CommentResponseDTO;
 import io.howstheairtoday.communitydomainrds.entity.Comment;
 
 @ActiveProfiles("test")
@@ -21,19 +26,34 @@ public class CommentServiceTest {
     @Autowired
     private CommentService commentService;
 
-    @DisplayName("댓글 작성")
-    @Test
-    public void createCommentTest(){
+    private UUID postId;
 
-        //given
-        UUID postId = UUID.randomUUID();
-        CommentRequestDTO commentRequestDTO = CommentRequestDTO.builder()
+    private UUID commentId;
+
+    private UUID memberId;
+
+    private CommentRequestDTO commentRequestDTO;
+
+    private Comment savedComment;
+
+    @BeforeEach
+    void setUp() {
+
+        postId = UUID.randomUUID();
+        memberId = UUID.randomUUID();
+
+        commentRequestDTO = CommentRequestDTO.builder()
             .content("테스트 댓글")
             .memberId(UUID.randomUUID())
             .build();
 
         //when
-        commentService.createComment(postId, commentRequestDTO);
+        savedComment = commentService.createComment(postId, commentRequestDTO);
+    }
+
+    @DisplayName("댓글 작성")
+    @Test
+    public void createCommentTest(){
 
         //then
         assertNotNull(postId);
@@ -41,20 +61,23 @@ public class CommentServiceTest {
         assertNotNull(commentRequestDTO.getMemberId());
     }
 
+    @DisplayName("댓글 조회")
+    @Test
+    void getComment() {
+
+        //when
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("createdAt").descending());
+        Slice<CommentResponseDTO> result = commentService.getComment(postId, pageRequest);
+
+        //then
+        assertThat(result.getContent().get(0).getContent()).isEqualTo(commentRequestDTO.getContent());
+    }
+
     @DisplayName("댓글 수정")
     @Test
     void updateComment() {
 
-        // given
-        UUID postId = UUID.randomUUID();
-
-        CommentRequestDTO commentRequestDTO = CommentRequestDTO.builder()
-            .content("테스트 댓글2")
-            .memberId(UUID.randomUUID())
-            .build();
-
-        Comment savedComment = commentService.createComment(postId, commentRequestDTO);
-
+        //given
         CommentRequestDTO requestDTO = CommentRequestDTO.builder()
             .content("수정된 댓글")
             .build();
@@ -69,16 +92,6 @@ public class CommentServiceTest {
     @DisplayName("댓글 삭제")
     @Test
     void deletedComment() {
-
-        // given
-        UUID postId = UUID.randomUUID();
-
-        CommentRequestDTO commentRequestDTO = CommentRequestDTO.builder()
-            .content("테스트 댓글중")
-            .memberId(UUID.randomUUID())
-            .build();
-
-        Comment savedComment = commentService.createComment(postId, commentRequestDTO);
 
         // when
         Comment updatedComment = commentService.deleteComment(savedComment.getCommentId());
