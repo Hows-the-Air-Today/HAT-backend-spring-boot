54663df3ae53be15648ec5cf3cc7d2e251604893
jhva <junghun8158@naver.com>
2023-04-18 05:20:22 +0900

    ✨ feat: ErrorMessage enum추가

--- community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/exception/dto/ErrorMessage.java
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/exception/dto/ErrorMessage.java
@@ -11,7 +11,8 @@ public enum ErrorMessage {
 
     CONFLICT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버와 연결이 끊어졌습니다."),
 
-    NOT_EXIST_POST(HttpStatus.NOT_FOUND, "게시물이 존재하지 않습니다.");
+    NOT_EXIST_POST(HttpStatus.NOT_FOUND, "게시물이 존재하지 않습니다."),
+    NOT_MEMBER_ID(HttpStatus.NOT_FOUND, "멤버 아이디가 없습니다.");
 
     private final HttpStatus status;
     private final String description;
