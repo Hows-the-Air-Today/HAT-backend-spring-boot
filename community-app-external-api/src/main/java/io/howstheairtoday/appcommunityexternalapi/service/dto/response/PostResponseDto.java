7841b6acc670b09e9966b6808037a2f92da16d1e
김정훈 <88875539+jhva@users.noreply.github.com>
2023-04-26 16:10:05 +0900

    ♻️ refactor(HAT-332): 나의게시물결과값변경 (#70)
    * ♻️ refactor: response값 추가
    
    * ♻️ refactor: memberImageUrl추가

--- community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/service/dto/response/PostResponseDto.java
+++ community-app-external-api/src/main/java/io/howstheairtoday/appcommunityexternalapi/service/dto/response/PostResponseDto.java
@@ -37,6 +37,10 @@ public class PostResponseDto {
         private String region;
         private String imageUrl;
 
+        private String memberImageUrl;
+        private int commentCount;
+        private int likeCount;
+
         private LocalDateTime createdAt;
         private LocalDateTime updatedAt;
         private LocalDateTime deletedAt;
