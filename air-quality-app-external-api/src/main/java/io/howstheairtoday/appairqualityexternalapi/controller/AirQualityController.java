7f004dc52f1e1167fb6074f496fa51c5d8bf4614
Hyyena <sjtxm0320@gmail.com>
2023-04-27 10:08:21 +0900

    ✨ feat(HAT-337): 미세먼지/초미세먼지 조회 컨트롤러 추가
    1. `/api/v1/airquality/forecast`로 `GET` 요청 시 조회 가능
    
    2. API 공통 응답 DTO 추가

---
+++ air-quality-app-external-api/src/main/java/io/howstheairtoday/appairqualityexternalapi/controller/AirQualityController.java
@@ -0,0 +1,43 @@
+package io.howstheairtoday.appairqualityexternalapi.controller;
+
+import io.howstheairtoday.airqualitydomainrds.dto.response.CurrentDustResponseDTO;
+import io.howstheairtoday.appairqualityexternalapi.common.ApiResponse;
+import io.howstheairtoday.appairqualityexternalapi.service.AirQualityRealTimeService;
+import io.howstheairtoday.appairqualityexternalapi.service.PMForecastService;
+import io.howstheairtoday.appairqualityexternalapi.service.dto.response.PMForecastResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/airquality")
+public class AirQualityController {
+
+    private final AirQualityRealTimeService airQualityRealTimeService;
+    private final PMForecastService pmForecastService;
+
+    // 대기질 측정정보 조회
+    @GetMapping("/tm")
+    @ResponseBody
+    public Map<String, CurrentDustResponseDTO> selectAirQualityRealTime(@RequestParam("tmx") String tmX, @RequestParam("tmy") String tmY) {
+
+        // TM 좌표를 이용하여 근처 측정소 찾기
+        String stationName = airQualityRealTimeService.getNear(tmX, tmY);
+
+        // 데이터 값을 Map 으로 변환
+        Map<String, CurrentDustResponseDTO> data = new HashMap<>();
+        data.put("airQuality", airQualityRealTimeService.selectAirQualityRealTime(stationName));
+        return data;
+    }
+
+    @GetMapping("/forecast")
+    public ApiResponse<PMForecastResponse> getAllPMForecasts() {
+
+        PMForecastResponse pmForecastResponse = pmForecastService.getAllPMForecastData();
+
+        return ApiResponse.success("미세먼지/초미세먼지 주간 예보 정보 조회 성공", pmForecastResponse);
+    }
+}
